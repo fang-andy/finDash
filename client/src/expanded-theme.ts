10c53c190968b08c92981a0e18b71dde1f099e29
fang-andy <afang1094@gmail.com>
2023-06-16 14:46:44 -0700

    handling import error

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+// import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+import '@material-ui/core/styles';
 
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
